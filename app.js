ba0006dc7e3a206d0f5d8375602e8ced29bb772f
Alan Bahena <alanbahena1@gmail.com>
2022-01-19 13:12:58 -0800

    Web Front-End Done

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const hamburger = document.querySelector('.navbar__container-hamburguer');
-const navLinks = document.querySelector('.navbar__container-links');
-const links = document.querySelector('.navbar__container-links li');
-const line = document.querySelector('.line');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  line.classList.toggle("color");
-});
