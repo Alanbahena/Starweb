53b964247912ffe211749ccb2e0935281164afe1
Alan Bahena <alanbahena1@gmail.com>
2022-02-01 12:51:59 -0800

    New sercices proposal

--- server.js
+++ server.js
@@ -1,58 +1,55 @@
-const express  = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
-const path = require ("path");
+const path = require("path");
 const nodemailer = require("nodemailer");
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
 
 const { getMaxListeners } = require("process");
 const PORT = process.env.PORT || 3000;
-require ('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 //Middleware
-app.use(express.static("public"));  
+app.use(express.static("public"));
 app.use(express.json());
 
 //HOME
 
-app.get('/index', (req, res) => {
-    res.sendFile(__dirname + "/index.html"); 
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-
-
-app.post('/', (req, res)=> {
+app.post("/", (req, res) => {
   console.log(req.body);
 
   const transporter = nodemailer.createTransport({
     host: "smtpout.secureserver.net",
     port: 465,
-    secure: true, 
+    secure: true,
     auth: {
       user: process.env.EMAIL1,
-      pass: process.env.PASS1
-    }
+      pass: process.env.PASS1,
+    },
   });
 
   const mailOptions = {
     from: req.body.email,
-    to: 'info@starweb.services',
+    to: "info@starweb.services",
     subject: "Tienes un nuevo mensaje de un Cliente",
-    text: req.body.message
+    text: req.body.message,
   };
 
-  transporter.sendMail(mailOptions, (error, info)=> {
-    if(error){
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
       console.log(error);
-      res.send('error');
+      res.send("error");
     } else {
-      console.log('Email sent: ' + info.response)
-      res.send('success')
+      console.log("Email sent: " + info.response);
+      res.send("success");
     }
   });
-
 });
 
 let port = process.env.PORT;
@@ -60,6 +57,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, () => {          
-  console.log('Server has started succesfully');
+app.listen(port, () => {
+  console.log("Server has started succesfully");
 });
